305803b684c604ee9054a1f597e5cd8fb40de3d5
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-07-13 17:05:43 +0700

    Refactor AuthProvider to conditionally render children based on session status and add Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,30 @@
-'use client';
-
-import AuthPopUp from '@/components/authPopUp';
-import Aside from '@/components/aside';
 // import ShowSession from '@/components/showSession';
-import { useSession } from 'next-auth/react';
-import Header from '@/components/header';
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        <main className="flex h-full border-t">
-          <Aside />
-          <div className="h-full w-full overflow-y-auto bg-slate-100">
-            <Header />
-            <div className="p-10">
-              <span className="mb-4 block text-xl font-light">Today</span>
-              <div className="grid grid-cols-2 gap-5">
-                <div className="col-span-2 rounded-md bg-white p-4">
-                  Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Optio ratione tempora eaque officiis alias ut incidunt!
-                  Quaerat, saepe atque, aperiam ullam nisi ipsa deserunt
-                  obcaecati aliquam enim consectetur magnam iste!
-                </div>
-                <div className="rounded-md bg-white p-4">
-                  Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Optio ratione tempora eaque officiis alias ut incidunt!
-                  Quaerat, saepe atque, aperiam ullam nisi ipsa deserunt
-                  obcaecati aliquam enim consectetur magnam iste!
-                </div>
-                <div className="rounded-md bg-white p-4">
-                  Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Optio ratione tempora eaque officiis alias ut incidunt!
-                  Quaerat, saepe atque, aperiam ullam nisi ipsa deserunt
-                  obcaecati aliquam enim consectetur magnam iste!
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <ShowSession /> */}
-        </main>
-      </>
-    );
-  }
   return (
-    <div className="flex h-screen items-center justify-center p-4">
-      <AuthPopUp />
-    </div>
+    <main className="p-10">
+      <span className="mb-4 block text-xl font-light">Today</span>
+      <div className="grid grid-cols-2 gap-5">
+        <div className="col-span-2 rounded-md bg-white p-4">
+          Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          ratione tempora eaque officiis alias ut incidunt! Quaerat, saepe
+          atque, aperiam ullam nisi ipsa deserunt obcaecati aliquam enim
+          consectetur magnam iste!
+        </div>
+        <div className="rounded-md bg-white p-4">
+          Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          ratione tempora eaque officiis alias ut incidunt! Quaerat, saepe
+          atque, aperiam ullam nisi ipsa deserunt obcaecati aliquam enim
+          consectetur magnam iste!
+        </div>
+        <div className="rounded-md bg-white p-4">
+          Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          ratione tempora eaque officiis alias ut incidunt! Quaerat, saepe
+          atque, aperiam ullam nisi ipsa deserunt obcaecati aliquam enim
+          consectetur magnam iste!
+        </div>
+      </div>
+      {/* <ShowSession /> */}
+    </main>
   );
 }
--- src/components/authProvider.tsx
+++ src/components/authProvider.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
+
+import AuthPopUp from '@/components/authPopUp';
+import Aside from '@/components/aside';
+import Header from '@/components/header';
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object
@@ -10,5 +15,29 @@ export default function AuthProvider({
 }: {
   children?: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <Auth>{children}</Auth>
+    </SessionProvider>
+  );
+}
+
+function Auth({ children }: { children?: React.ReactNode }) {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <div className="flex h-full border-t">
+        <Aside />
+        <div className="h-full w-full overflow-y-auto bg-slate-100">
+          <Header />
+          {children}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="flex h-screen items-center justify-center p-4">
+      <AuthPopUp />
+    </div>
+  );
 }
