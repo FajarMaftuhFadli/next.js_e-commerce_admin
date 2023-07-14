43ecc8bdfab33deedb18724ba8db8300261e8a5d
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-07-14 09:17:22 +0700

    Add new pages for help, orders, and settings with corresponding titles in the page component

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+import useStore from '@/stores/store';
+
+import AuthCheckWrapper from '@/components/authCheckWrapper';
+
+export default function Products() {
+  const setPageTitle = useStore((state) => state.setPageTitle);
+
+  useEffect(() => {
+    setPageTitle('orders');
+  }, [setPageTitle]);
+
+  return (
+    <AuthCheckWrapper>
+      <main className="p-10">Orders</main>
+    </AuthCheckWrapper>
+  );
+}
